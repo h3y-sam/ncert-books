7cf93f4342a9ec95b332b8fff8bcacafb5dac962
h3y-sam <h3y.samm@gmail.com>
2025-06-30 01:08:21 +0530

    index.js

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-
-  function toggleTheme() {
-    document.body.classList.toggle('dark-mode');
-    const themeBtn = document.getElementById('theme-toggle');
-    if (document.body.classList.contains('dark-mode')) {
-      themeBtn.innerText = "☀️ Light Mode";
-    } else {
-      themeBtn.innerText = "🌙 Dark Mode";
-    }
+function toggleTheme() {
+  document.body.classList.toggle('dark-mode');
+  const btn = document.getElementById('theme-toggle');
+  if(document.body.classList.contains('dark-mode')) {
+    btn.textContent = '☀️';
+  } else {
+    btn.textContent = '🌙';
   }
-
-   
+}
